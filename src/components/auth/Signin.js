bcd5bf06670f975137fbd92e54219bd2891058ba
leoni <leonimega@hotmail.com>
2017-12-23 10:13:57 -0200

    add more stuff

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
-import {reduxForm} from "redux-form";
-
+import {connect} from "react-redux";
+import {Field, reduxForm} from "redux-form";
+import * as actions from "../../actions";
 class Signin extends Component {
 
-  render() {
+  handleFormSubmit({email, password}) {
+    console.log(email, password);
+    this.props.signinUser({email, password});
+  }
+
+  renderField(field){
     return (
-      <form>
-        <fieldSet className="form-group">
-          <label>Email:</label>
-          <input className="form-control"/>
-        </fieldSet>
-        <fieldSet className="form-group">
-          <label>Password:</label>
-          <input className="form-control"/>
-        </fieldSet>
+      <div>
+        <input {...field.input} type={field.type} className="form-control" />
+        {field.meta.touched && field.meta.error}
+        <span>{field.meta.error}</span>
+      </div>
+    );
+  }
+
+  render() {
+    const {handleSubmit, fields: {email, password}} = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+      <fieldset className="form-group">
+        <label>Email</label>
+        <Field
+        name="email"
+        component={this.renderField}
+        type="text" />
+      </fieldset>
+      <fieldset className="form-group">
+        <label>Password</label>
+        <Field
+        name="password"
+        component={this.renderField}
+        type="text" />
+      </fieldset>
         <button action="submit" className="btn btn-primary">Sign In</button>
       </form>
     );
   }
-
 }
 
-export default reduxForm({
+export default connect(null, actions)(
+  reduxForm({
   form: "signin",
   fields: ["email", "password"]
-})(Signin);
+  })(Signin)
+);
--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-
+import reduxThunk from "redux-thunk";
 import App from './components/App';
+import Signin from './components/auth/Signin';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
         <div>
             <Switch>
+                <Route path="/signin" component={Signin}/>
                 <Route path="/" component={App}/>
             </Switch>
         </div>
